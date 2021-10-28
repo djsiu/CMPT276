3f1ce1ce7a289a66ebc908751d50b30bee8c87de
Peter Soava <pgs2@sfu.ca>
2021-10-27 23:40:55 -0700

    Initial Commit

---
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/MainMenu.java
@@ -0,0 +1,16 @@
+package com.cmpt276.calciumparentapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmpt276.calciumparentapp.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
