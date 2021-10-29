0bcb1f7366d0a6628a6aec2aa2911958f6acbdb0
Peter Soava <pgs2@sfu.ca>
2021-10-29 00:41:38 -0700

    Add TextView title to each activity and fragment

--- app/src/main/java/com/cmpt276/calciumparentapp/ui/MainMenu.java
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/MainMenu.java
@@ -11,6 +11,6 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
     }
 }
--- app/src/main/java/com/cmpt276/calciumparentapp/ui/manage/ManageFamilyAdd.java
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/manage/ManageFamilyAdd.java
@@ -1,66 +1,16 @@
 package com.cmpt276.calciumparentapp.ui.manage;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.cmpt276.calciumparentapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ManageFamilyAdd#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ManageFamilyAdd extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ManageFamilyAdd() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ManageFamilyAdd.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ManageFamilyAdd newInstance(String param1, String param2) {
-        ManageFamilyAdd fragment = new ManageFamilyAdd();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class ManageFamilyAdd extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_family_add, container, false);
+        setContentView(R.layout.activity_manage_family_add);
     }
 }
