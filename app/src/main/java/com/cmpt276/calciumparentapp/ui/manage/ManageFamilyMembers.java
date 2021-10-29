d431aef181f5bf5fd334c7b452acbd0406bef73b
Peter Soava <pgs2@sfu.ca>
2021-10-29 01:59:42 -0700

    Add floating action button to open ManageFamilyAdd

--- app/src/main/java/com/cmpt276/calciumparentapp/ui/manage/ManageFamilyMembers.java
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/manage/ManageFamilyMembers.java
@@ -7,8 +7,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.cmpt276.calciumparentapp.R;
+import com.cmpt276.calciumparentapp.ui.MainMenu;
+import com.cmpt276.calciumparentapp.ui.coinflip.CoinFlip;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ManageFamilyMembers extends AppCompatActivity {
 
@@ -17,10 +21,22 @@ public class ManageFamilyMembers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_family_members);
 
+        FloatingActionButton btnAddMember = findViewById(R.id.manage_family_add_button);
+
+        setupManageFamilyAddButton(btnAddMember);
+
         //Adds back button in top left corner
         ActionBar ab = getSupportActionBar();
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupManageFamilyAddButton(FloatingActionButton button) {
+        button.setOnClickListener(v -> {
+            // Opens the ManageFamilyAdd activity
+            Intent i = ManageFamilyAdd.makeIntent(ManageFamilyMembers.this);
+            startActivity(i);
+        });
     }
 
     /**
