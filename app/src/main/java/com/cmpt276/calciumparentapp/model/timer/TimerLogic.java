cadd30f492be3ae3ba121d3f9a971bad7404e881
Peter Soava <pgs2@sfu.ca>
2021-10-31 16:29:36 -0700

    Add implementation for timer counting down

---
+++ app/src/main/java/com/cmpt276/calciumparentapp/model/timer/TimerLogic.java
@@ -0,0 +1,4 @@
+package com.cmpt276.calciumparentapp.model.timer;
+
+public class TimerLogic {
+}
--- app/src/main/java/com/cmpt276/calciumparentapp/ui/timer/Timer.java
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/timer/Timer.java
@@ -6,21 +6,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.cmpt276.calciumparentapp.R;
 
 public class Timer extends AppCompatActivity {
+    private TextView countdownText;
+
+    private CountDownTimer countDownTimer;
+    private long timeInMS = 50000; // 5 seconds
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
+        countdownText = findViewById(R.id.countdown_text);
+
         //Adds back button in top left corner
         ActionBar ab = getSupportActionBar();
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
+
+        startTimer();
     }
 
     /**
@@ -38,8 +49,37 @@ public class Timer extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeInMS, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeInMS = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        timerRunning = true;
+    }
+
+    public void updateTimer(){
+        int minutes = (int) timeInMS / 60000;
+        int seconds = (int) timeInMS % 60000 / 1000;
+
+        String timeLeftText;
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+
+        countdownText.setText(timeLeftText);
+    }
+
     public static Intent makeIntent(Context context){
         return new Intent(context, Timer.class);
     }
-
 }
