824ebb91c1691b2926c6d1cbb8b1cb60257d9291
Matthew <msa245@sfu.ca>
2021-11-03 13:19:51 -0700

    Change custom time to activity from fragment

--- app/src/main/java/com/cmpt276/calciumparentapp/ui/timer/TimerCustomTime.java
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/timer/TimerCustomTime.java
@@ -2,6 +2,7 @@ package com.cmpt276.calciumparentapp.ui.timer;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,7 +11,7 @@ import android.view.ViewGroup;
 
 import com.cmpt276.calciumparentapp.R;
 
-public class TimerCustomTime extends Fragment {
+public class TimerCustomTime extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
