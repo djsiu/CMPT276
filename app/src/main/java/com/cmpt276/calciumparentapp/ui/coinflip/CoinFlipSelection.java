0f2af48c22435b76928f614bfcb2492b76569337
Braden Takashima <bradentakashima@gmail.com>
2021-11-06 00:58:10 -0700

    Working version of list, Must code it to only accept two entries.

---
+++ app/src/main/java/com/cmpt276/calciumparentapp/ui/coinflip/CoinFlipSelection.java
@@ -0,0 +1,117 @@
+package com.cmpt276.calciumparentapp.ui.coinflip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.cmpt276.calciumparentapp.R;
+import com.cmpt276.calciumparentapp.model.manage.FamilyMembersManager;
+import com.cmpt276.calciumparentapp.ui.manage.ManageFamilyEdit;
+import com.cmpt276.calciumparentapp.ui.manage.ManageFamilyMembers;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+// array of options --> array adapter --> ListView
+
+//List View: {views: recycler_name_list_items.xml}
+
+public class CoinFlipSelection extends AppCompatActivity {
+    
+    private String TAG = "CoinFlipSelection";
+
+    private ArrayList<String> nameArrayList = new ArrayList<String>();
+
+    private FamilyMembersManager familyMembersManager;
+
+    private int selected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip_selection);
+
+        populateListView();
+        selected = 0;
+
+        Button button = findViewById(R.id.coin_selection_button_continue);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = CoinFlip.makeIntent(CoinFlipSelection.this);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+    private void populateArray(){
+        nameArrayList.add("a");
+        nameArrayList.add("b");
+        nameArrayList.add("c");
+        nameArrayList.add("d");
+
+    }
+
+    private void populateListView() {
+        //create List of items
+        populateArray();
+
+        //Build adapter
+        ArrayAdapter<String> adapter = new MyListAdapter();
+        Log.i(TAG, "populateListView: After build adapter");
+
+        ListView list = (ListView) findViewById(R.id.coin_list_names);
+        Log.i(TAG, "populateListView: assign list");
+        list.setAdapter(adapter);
+        Log.i(TAG, "populateListView: set adapter");
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String> {
+        public MyListAdapter() {
+            super(CoinFlipSelection.this, R.layout.recycler_name_list_items, nameArrayList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //Make sure we have a view to work with (may have been given null
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.recycler_name_list_items, parent, false);
+            }
+
+            //find the name
+            String name = nameArrayList.get(position);
+
+            //fill the view
+            Log.i(TAG, "getView: Fill view");
+            TextView textView = itemView.findViewById(R.id.textView_list_name);
+            Log.i(TAG, "getView: name is \n\n\n" + name + "\n\n\n ");
+            if(textView == null){
+                Log.i(TAG, "getView: tv was null");
+            }else{
+                Log.i(TAG, "getView: set "+ name);
+                textView.setText(name);
+            }
+
+
+            return itemView;
+        }
+    }
+
+    public static Intent makeIntent(Context context){
+        return new Intent(context, CoinFlipSelection.class);
+    }
+}
